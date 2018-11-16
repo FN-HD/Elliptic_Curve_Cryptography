54947c664b911e307799b5b435f44a88f9134035
hide funa <apollon.jack.com@gmail.com>
2018-11-17 08:05:48 +0900

    edit main

---
+++ Main.py
@@ -0,0 +1,21 @@
+from Mods.EC.Elliptic_Curve import EllipticCurve
+from Mods.EC.Rational_Point_In_EC import RationalPointInEC
+
+print(EllipticCurve.get_instance(0, -2))
+
+o = RationalPointInEC(0, 0)
+r1 = RationalPointInEC(-1, 1)
+r2 = RationalPointInEC(2, 2)
+
+print('show value')
+print('o = ' + str(o))
+print('r1 = '+str(r1))
+print('r2 = '+str(r2))
+
+print('show calculation')
+print('r1 + o = '+str(r1 + o))
+print('r2 + r1 = '+str(r2 + r1))
+print('2 * r1 = '+str(2*r1))
+print('\'4*r1 == 3*r1 + r1\' is ' + str(4*r1 == 3*r1 + r1))
+
+
